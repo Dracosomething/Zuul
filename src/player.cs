986c249b1dbbafa0e82f57c2cc51fbd8da17e3ac
Dracosomething <larsfalk08@gmail.com>
2025-02-25 08:50:03 +0100

    added heal and damage methods to player class

--- src/player.cs
+++ src/player.cs
@@ -2,8 +2,22 @@
 
 class Player {
     public Room CurrentRoom { get; set; }
+    private int Health;
 
     public Player() {
         CurrentRoom = null;
+        Health = 100;
     }
+
+    public void Damage(int amount)
+    {
+        Health -= amount;
+    }
+
+    public void Heal(int amount)
+    {
+        Health += amount;
+    }
+    
+    
 }
