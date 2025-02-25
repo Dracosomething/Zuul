c7cc2e17233a035d79d5a1b13873e36c580dc926
Dracosomething <larsfalk08@gmail.com>
2025-02-25 12:22:52 +0100

    added Item class

---
+++ src/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace Zuul;
+
+class Item {
+    // fields
+    private int weight;
+    private string description;
+    
+    // attributes
+    public int Weight { get; }
+    public string Description { get; }
+
+    // constructor
+    public Item(int weight, string description) {
+        weight = weight;
+        description = description;
+    }
+}
