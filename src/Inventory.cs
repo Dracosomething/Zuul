a2b76f29d7475f2a30ad13bc1a5584c676173991
Dracosomething <larsfalk08@gmail.com>
2025-02-25 13:22:35 +0100

    added Inventory class with the methods get, put, totalweight and freeweight

---
+++ src/Inventory.cs
@@ -0,0 +1,42 @@
+﻿namespace Zuul;
+
+class Inventory {
+    // fields
+    private int maxWeight;
+    private Dictionary<string, Item> items;
+    
+    // constructor
+    public Inventory(int maxWeight) {
+        this.maxWeight = maxWeight;
+        this.items = new Dictionary<string, Item>();
+    }
+    
+    // methods
+    public bool Put(string itemName, Item item) {
+        int itemWeight = item.Weight;
+        if (itemWeight <= FreeWeight()) {
+            items.Add(itemName, item);
+            return true;
+        }
+        return false;
+    }
+
+    public Item Get(string itemName) {
+        if (items.ContainsKey(itemName)) {
+            return items[itemName];
+        }
+        return null;
+    }
+
+    public int TotalWeight() {
+        int total = 0;
+        foreach (var keyValuePair in items) {
+            total += keyValuePair.Value.Weight;
+        }
+        return total;
+    }
+
+    public int FreeWeight() {
+        return maxWeight - TotalWeight();
+    }
+}
