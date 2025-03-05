c112b9dea8ec864d619a6307067bd3fe51e798f4
Dracosomething <larsfalk08@gmail.com>
2025-03-05 11:47:25 +0100

    added trap class

---
+++ src/Trap.cs
@@ -0,0 +1,58 @@
+namespace Zuul;
+
+class Trap : Entity {
+    // fields
+    private string name;
+    private string describtion;
+    private bool hasNextRoom;
+    private Room nextRoom;
+    private Action function;
+    
+    // properties
+    public Action Function { set { this.function = value; } }
+    public string Name { get { return name; } }
+    
+    // constructor
+    public Trap(int damage, string name, string describtion, bool hasNextRoom, Action func) : base(damage, 0, 0) {
+        this.name = name;
+        this.describtion = describtion;
+        this.hasNextRoom = hasNextRoom;
+        this.nextRoom = null;
+        this.function = func;
+    }
+    
+    public Trap(int damage, string name, string describtion, bool hasNextRoom) : base(damage, 0, 0) {
+        this.name = name;
+        this.describtion = describtion;
+        this.hasNextRoom = hasNextRoom;
+        this.nextRoom = null;
+        this.function = null;
+    }
+    
+    public Trap(int damage, string name, string describtion, bool hasNextRoom, Room nextRoom, Action func) : base(damage, 0, 0) {
+        this.name = name;
+        this.describtion = describtion;
+        this.hasNextRoom = hasNextRoom;
+        this.nextRoom = nextRoom;
+        this.function = func;
+    }
+    
+    // methods
+    public void Disarm() {
+        this.Discard();
+        this.CurrentRoom = null;
+        Console.WriteLine($"Successfully disarmed {this.name}");
+    }
+
+    public void Discard() {
+        this.CurrentRoom.Inhabitants.Remove(this.name);
+    }
+
+    public void Tick() {
+        foreach (var currentRoomInhabitant in this.CurrentRoom.Inhabitants) {
+            if (!(currentRoomInhabitant.Value is Trap)) {
+                this.function.Invoke();
+            }
+        }
+    }
+}
